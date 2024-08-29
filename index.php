bdfb71584bcfe971c31b9b0870fbad01d985b73c
KaikService <144274075+KaiqueKrewer@users.noreply.github.com>
2024-08-29 19:34:59 -0300

    Allfuncions

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once 'admin.php';
 
 echo "<h1>Biblioteca</h1>";
-echo "<p>Bem-vindo à nossa biblioteca!</p>";
 echo "<p><a href='create.php'>Cadastrar novo livro</a></p>";
 echo "<p><a href='read.php'>Ver todos os livros</a></p>";
 echo "<p><a href='delete.php'>Deletar livro</a></p>";
