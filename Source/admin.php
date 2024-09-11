e9e93402e8465265818865db4d5b7c47b4009604
Calixto <161600638+Nyawuriny@users.noreply.github.com>
2024-09-10 22:48:33 -0300

    Add files via upload
    CSS implementado

--- Source/admin.php
+++ Source/admin.php
@@ -1,25 +1,7 @@
 <?php
-require_once '../Source/admin.php';
+$conexao = mysqli_connect("localhost", "root", "", "biblioteca");
 
-// Conexão com o servidor MySQL
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "biblioteca";
-
-// Criar conexão
-$conexao = mysqli_connect($servername, $username, $password, $dbname);
-
-// Verificar conexão
 if (!$conexao) {
-    die("Erro ao conectar ao servidor MySQL: " . mysqli_connect_error());
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $titulo = $_POST["titulo"] ?? null;
-
-    if ($titulo !== null) {
-        $titulo = mysqli_real_escape_string($conexao, $titulo);
-    }
+    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
 }
 ?>
--- Source/index.php
+++ Source/index.php
@@ -3,25 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biblioteca - XXXXXX</title> <?php // ve ai um nome ?>
-    <link rel="stylesheet" href="style.css">
+    <title>Biblioteca - Trucou</title> <?php // ve ai um nome ?>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    <?php
-    echo "<h1>Biblioteca</h1>";
-    echo  "<p>livros</p>";
-    echo "<p><a href='../crudBook/create.php'>Cadastrar novo livro</a></p>";
-    echo "<p><a href='../crudBook/read.php'>Ver todos os livros</a></p>";
-    echo "<p><a href='../crudBook/delete.php'>Deletar livro</a></p>";
-    echo "<p><a href='../crudBook/update.php'>Editar livro</a></p>";
-    echo "<br></br>";
-    echo "<p>Autores</p>";
-    echo "<p><a href='../crudAutor/createAutor.php'>Cadastrar autor</a></p>";
-    echo "<p><a href='../crudAutor/readAutor.php'>Listar autores</a></p>";
-    echo "<p><a href='../crudAutor/deleteAutor.php'>Deletar autor</a></p>";
-    echo "<p><a href='../crudAutor/updateAutor.php'>Editar autor</a></p>";
-    ?>
-    
+    <div class="container">
+            <h1>Biblioteca Trucou</h1>
+        <?php
+        require_once 'admin.php';
+        echo  "<p>Livros: </p>";
+        echo "<p><a href='../crudBook/create.php'>Cadastrar novo livro</a></p>";
+        echo "<p><a href='../crudBook/read.php'>Ver todos os livros</a></p>";
+        echo "<p><a href='../crudBook/delete.php'>Deletar livro</a></p>";
+        echo "<p><a href='../crudBook/update.php'>Editar livro</a></p>";
+        echo "<br></br>";
+        echo "<p>Autores: </p>";
+        echo "<p><a href='../crudAutor/createAutor.php'>Cadastrar autor</a></p>";
+        echo "<p><a href='../crudAutor/readAutor.php'>Listar autores</a></p>";
+        echo "<p><a href='../crudAutor/deleteAutor.php'>Deletar autor</a></p>";
+        echo "<p><a href='../crudAutor/updateAutor.php'>Editar autor</a></p>";
+        ?>
+    </div>
 </body>
 </html>
 
